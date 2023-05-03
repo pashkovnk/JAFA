9fb72aa426ce91262d9c2ac4378db83213561feb
pashkovnk <pashkov.nikita03@mail.ru>
2023-05-03 03:30:18 +0300

    Изменения: - Переделал сплэшскрин - Удалил activity_splash_screen.xml за ненадобностью

--- app/src/main/java/com/example/jafa/SplashScreen.java
+++ app/src/main/java/com/example/jafa/SplashScreen.java
@@ -4,39 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
-
-    Animation logoAnimation;
-    ImageView imageView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
 
-
-
-
-
-        ImageView logo = findViewById(R.id.logosplash);
-        logoAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.logo_anim_splashscreen);
-        logo.setAnimation(logoAnimation);
-
-
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-//                startActivity(new Intent(getApplicationContext(), MainActivity.class));
-//                finish();
-
-            }
-        },1800);
-
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
